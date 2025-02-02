1426749938b5d338056a57b903181c04fc6ca058
Joakinn014 <j.olivaresalvo0607@gmail.com>
2025-02-02 19:59:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-temp = ""
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "DAMN": "Asombroso",
+            "POV": "Punto de vista raro"
+            }
 
-def hola():
-    global temp
-    list=["H", "o", "l", "a"]
+pregunta = input("Ingrese la palabra que quiera conocer: ")
 
-    for i in list:
-        temp += i
-
-    print(temp)
-
-hola()
+for palabra in meme_dict.keys():
+    if pregunta == palabra:
+        print(f"El significado de la palabra {pregunta} es {meme_dict[palabra]}")
